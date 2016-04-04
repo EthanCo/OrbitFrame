aff4a99d13304f34b1f697e9d7e60e41e88b6685
朱豪凯 <1406472408@qq.com>
2016-04-04 23:30:45 +0800

    ...

--- app/src/main/java/com/ethanco/simpleframesample/view/LoginActivityComponent.java
+++
@@ -1,9 +0,0 @@
-package com.ethanco.simpleframesample.view;
-
-/**
- * Created by EthanCo on 2016/4/4.
- */
-//@Component(modules = LoginActivityModule.class)
-//public interface LoginActivityComponent {
-//    void inject(LoginActivity activity);
-//}
