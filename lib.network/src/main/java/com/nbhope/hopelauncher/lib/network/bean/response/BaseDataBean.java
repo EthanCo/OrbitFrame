9ecdd7f24f7c15246ebb5c8c38298eef356f763e
EthanCo <fenghenyu@163.com>
2017-10-10 13:42:00 +0800

    delete

--- lib.network/src/main/java/com/nbhope/hopelauncher/lib/network/bean/response/BaseDataBean.java
+++
@@ -1,9 +0,0 @@
-package com.nbhope.hopelauncher.lib.network.bean.response;
-
-/**
- * @Description DataBean基类，用于错误时多出的Message
- * Created by EthanCo on 2016/7/6.
- */
-public interface BaseDataBean {
-    String getMessage();
-}
--- lib.network/src/main/java/com/nbhope/hopelauncher/lib/network/observer/Observer.java
+++
@@ -1,9 +0,0 @@
-package com.nbhope.hopelauncher.lib.network.observer;
-
-/**
- * @Description Observer
- * Created by EthanCo on 2016/7/15.
- */
-public interface Observer {
-    void notify(Type type);
-}
